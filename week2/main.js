4a678c36fd21d5f972db367747e527a49e748b1d
rserota <raphael.serota@gmail.com>
2015-06-23 10:11:12 -0600

    intro to functions

--- week2/main.js
+++ week2/main.js
@@ -90,7 +90,7 @@ else if ( age >= 18 && (( country === 'Ireland' ) || ( country === 'Norway' )) )
 // alert('Will you be served? ' + canDrink);
 
 // Array literal
-var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+// var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 // console.log(days.length)
 
@@ -121,21 +121,21 @@ var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 
 
 // Object literal
 // name:value pair.  or key:value pair.
-var sharkNado = {
-    title  : 'Sharknado II',
-    genre  : 'Science Fiction',
-    rating : 8,
-};
-var HTTM = {
-    title  : 'Hot Tub Time-Machine',
-    genre  : 'Historical autiobiography',
-    rating : 11,
-}
-var movies = [];
-movies.push(sharkNado);
-movies.push(HTTM);
+// var sharkNado = {
+//     title  : 'Sharknado II',
+//     genre  : 'Science Fiction',
+//     rating : 8,
+// };
+// var HTTM = {
+//     title  : 'Hot Tub Time-Machine',
+//     genre  : 'Historical autiobiography',
+//     rating : 11,
+// }
+// var movies = [];
+// movies.push(sharkNado);
+// movies.push(HTTM);
 
-console.log(movies)
+// console.log(movies)
 // HTTM['title']
 
 // var property = prompt('Which property do we want to check?');
@@ -152,3 +152,74 @@ console.log(movies)
 //     console.log('The ' + key + ' of the movie is ' + HTTM[key] + '.');
 // }
 
+
+// A function is a reusable block of code ( set of instructions ) that takes inputs, and produces outputs
+
+// Function another non-primitive type
+
+// Function literal
+var calculateArea = function(height, width){ // inputs are called arguments, or parameters
+    var area = height * width; // return defines the outputs of a function
+    return area
+}
+
+// console.log(calculateArea(4, 3)) // PASSING 4 and 3 into the function
+
+calculateArea(4, 3) // function call. we're invoking the function.
+calculateArea // function reference
+
+var anotherReference = calculateArea // this creates a second sign-post pointing to the same function
+var squareFeet = anotherReference(100,200) // the return statement lets us know what is ultimately assigned to squareFeet
+
+// myFunction = function(){
+
+// }
+
+// var steve = {}
+// var janet = {}
+
+// steve.doSomething = myFunction
+// janet.alsoDoSOmething = myFunction
+
+// steve.doSomething() // calling a method
+// var pluralize = function(word, number){
+//     if ( number === 1 ) {
+//         return [word, 'hello']; // use an array or object to return multiple values
+//         console.log('You will never see this.') // you can't see this if it's after the return statement
+//     }
+//     else {
+//         return word + 's'; // only one of these return statements will run
+//     }
+// }
+// console.log(pluralize('chair', 1))
+
+// console.log(pluralize('mongeese', 2))
+// var hello = 'hello!'
+// console.log(hello.toUpperCase()) // toUpperCase is both a function and a property, therefore it is also called a method
+
+// var weekString = 'Monday Tuesday Wednesday Thursday Friday Saturday Sunday';
+// var weekArray = weekString.split(' ');
+// console.log(weekArray);
+
+// weekString = weekArray.join(', ')
+// console.log(weekString);
+
+// functions create scope
+var concat = function(word1, word2){
+    var output = word1 + ' ' + word2;
+    return output;
+};
+
+var sentence = concat('Write', 'code!');
+console.log(sentence);
+console.log(output)
+// console.log(word1) // word1 is NOT defined here
+// console.log(output) // also not defined
+
+// var globalVariable = 5;
+// var whatsGlobal = function(){
+//     return globalVariable;
+// }
+// console.log(whatsGlobal()); // functions can see values defined outside of themselves, but code outside of a function cannot see into the function.
+
+'hello'.toUpperCase()
