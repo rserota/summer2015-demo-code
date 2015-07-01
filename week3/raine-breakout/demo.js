7c8da69997949ab00f60d9fe320fec8e4179dd07
metaraine <raineorshine@gmail.com>
2015-07-01 11:04:44 -0600

    week3/raine-breakout

---
+++ week3/raine-breakout/demo.js
@@ -0,0 +1,10 @@
+// Welcome to Raine's Week 3 Breakout Group!!!
+/*
+- scope, closure
+- functional programming
+- nested loops
+- functional programming in event-driven code
+- control flow
+- higher-order functions
+*/
+
