9d32466f72ceb14cf9cdf2d3987769ce47c93453
devaio <robert.camp@outlook.com>
2015-08-04 11:23:00 -0600

    comments

--- week8/angular-demo/public/main.js
+++ week8/angular-demo/public/main.js
@@ -3,7 +3,6 @@ var cryptApp = angular.module('cryptApp', ['ngResource', 'ngRoute']);
 
 // Route provider to control what controllers / views are used at a specific route
 cryptApp.config(function($routeProvider){
-
 	$routeProvider
 		.when('/', {
 			templateUrl : '/templates/cryptanimallist',
@@ -12,7 +11,12 @@ cryptApp.config(function($routeProvider){
 
 });
 
+// AnimalFactory creation
 cryptApp.factory('animalFactory', function($resource){
+
+	// This creates a $resource model
+	// Our base URL is /api/animals with the option of additionally passing the /:id component
+	// All the methods this $resource model uses will be in reference to those URLs
 	var model = $resource('/api/animals/:id', {id : '@_id'})
 	// this._id
 	// @_id
@@ -22,42 +26,43 @@ cryptApp.factory('animalFactory', function($resource){
 	// model.$save() // POST - /api/animals
 	// model.$delete()
 	// model.get({id : ObjectId('5483292394823')}) // GET - /api/animals/5483292394823
+
+	// Factories use the revealing module pattern, so we must return the relevant pieces of information
 	return {
 		model   : model,
-		animals : model.query() // GET - /api/animals
+		animals : model.query() // GET - /api/animals  Should get all the animals in our DB
 	}
 
 
 });
 
-// $.ajax({
-// 	url : '/api/animals',
-// 	method : 'GET',
-// 	success : function(returnData){}
-// })
-
-// $.ajax({
-// 	url : '/api/animals',
-// 	method : 'POST',
-// 	data : {asasdad}
-// })
-
-
-// cryptAnimalList Controller
+// cryptAnimalList Controller - we are injected animalFactory into it!
 cryptApp.controller('cryptAnimalList', function($scope, animalFactory){
 	console.log('I AM THE CONTROLLER', animalFactory);
 
+	// List of all of our animals returned from animalFactory.animals
 	$scope.animals = animalFactory.animals;
 
+	// AddAnimal function being called on a submit event in our template's form
 	$scope.addAnimal = function(){
-		console.log(this.newAnimal);
+		// console.log(this.newAnimal);
+
+		// Create a new instance of our $resource model to attach methods
+		// These methods include things like $save, $delete, get, etc
 		var newCryptAnimal = new animalFactory.model(this.newAnimal);
 
+		// Want to initiate the $save method
+		// This POSTS to /api/animals which will use the newCryptAnimal data to create a new database document
 		newCryptAnimal.$save(function(returnData){
 			// console.log('return', returnData)
+
+			// After we receive the saved document from the server, we will push it into our list of animals
+			// This allows the front-end to update immediately
 			animalFactory.animals.push(returnData);
 		});
 
+		// Our form inputs are modeling this object, by setting it back to an empty object, 
+		// we reset the values of all the forms (and the value of the newAnimal object)
 		this.newAnimal = {};
 
 	}
